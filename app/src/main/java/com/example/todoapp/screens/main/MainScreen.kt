8d71adf2f28da7eba422268e1e4a72fa4151b8c4
Gabriel Willans <g.willans@outlook.com>
2021-08-31 18:47:29 -0300

    main screen made

---
+++ app/src/main/java/com/example/todoapp/screens/main/MainScreen.kt
@@ -0,0 +1,32 @@
+package com.example.todoapp.screens.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todoapp.R
+
+class MainScreen : Fragment() {
+
+    companion object {
+        fun newInstance() = MainScreen()
+    }
+
+    private lateinit var viewModel: MainScreenViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.main_screen_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainScreenViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/todoapp/screens/main/MainScreenViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.todoapp.screens.main
+
+import androidx.lifecycle.ViewModel
+
+class MainScreenViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
