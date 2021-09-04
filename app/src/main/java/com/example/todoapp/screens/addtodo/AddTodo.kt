7d586a1129390d8d3161652264fda59758a2ead1
Gabriel Willans <g.willans@outlook.com>
2021-09-04 16:25:18 -0300

    addTodo Screen features implemented

--- app/src/main/java/com/example/todoapp/screens/addtodo/AddTodo.kt
+++ app/src/main/java/com/example/todoapp/screens/addtodo/AddTodo.kt
@@ -6,7 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
+import com.example.todoapp.databinding.AddTodoFragmentBinding
+import com.example.todoapp.utils.CalendarUtils
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.util.*
 
 class AddTodo : Fragment() {
 
@@ -15,18 +24,83 @@ class AddTodo : Fragment() {
     }
 
     private lateinit var viewModel: AddTodoViewModel
+    private lateinit var binding: AddTodoFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.add_todo_fragment, container, false)
+    ): View {
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.add_todo_fragment,container,false)
+        setOnClickListeners()
+        setOnChangeTextListeners()
+
+        viewModel = ViewModelProvider(this).get(AddTodoViewModel::class.java)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AddTodoViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun setOnChangeTextListeners(){
+        binding.titleTextField.editText?.addTextChangedListener {
+            if(it?.length==0) binding.titleTextField.error = "Digite um titulo"
+            else binding.titleTextField.error = null
+
+        }
+        binding.dateTextField.editText?.addTextChangedListener {
+            if(it?.length==0) binding.dateTextField.error = "Selecione uma data"
+            else binding.dateTextField.error = null
+
+        }
+        binding.hourTextField.editText?.addTextChangedListener {
+            if(it?.length==0) binding.hourTextField.error = "Selecione uma horário"
+            else binding.hourTextField.error = null
+
+        }
+    }
+
+    private fun setOnClickListeners(){
+        binding.dateTextField.editText?.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker()
+                                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                                .build()
+            datePicker.addOnPositiveButtonClickListener {
+                val timeZone = TimeZone.getDefault()
+                val offset = timeZone.getOffset(Date().time)*(-1)
+                val date = Calendar.getInstance()
+                date.timeInMillis = it + offset
+                val dateString = CalendarUtils.getDateString(date)
+                binding.dateTextField.editText?.setText(dateString)
+            }
+            datePicker.show(childFragmentManager,"DATE_PICKER_TAG")
+        }
+
+        binding.hourTextField.editText?.setOnClickListener {
+            val hourPicker = MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_24H).build()
+            hourPicker.addOnPositiveButtonClickListener {
+                val hour = if(hourPicker.hour<10) "0${hourPicker.hour}" else hourPicker.hour
+                val minute = if(hourPicker.minute<10) "0${hourPicker.minute}" else hourPicker.minute
+                val hourString = "$hour:$minute"
+                binding.hourTextField.editText?.setText(hourString)
+            }
+            hourPicker.show(childFragmentManager,"HOUR_PICKER_TAG")
+        }
+
+        binding.addTaskBtn.setOnClickListener {
+            val isTitleEmpty = binding.titleTextField.editText?.text?.length==0
+            val isDateEmpty = binding.dateTextField.editText?.text?.length==0
+            val isHourEmpty = binding.hourTextField.editText?.text?.length==0
+            if(isTitleEmpty || isDateEmpty || isHourEmpty) {
+                if(isTitleEmpty) binding.titleTextField.error = "Digite um titulo"
+                if(isDateEmpty) binding.dateTextField.error = "Selecione uma data"
+                if(isHourEmpty) binding.hourTextField.error = "Selecione uma horário"
+                return@setOnClickListener
+            }
+
+            findNavController().navigate(R.id.action_addTodo_to_mainScreen)
+        }
+
+        binding.cancelAddBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_addTodo_to_mainScreen)
+        }
     }
 
 }
