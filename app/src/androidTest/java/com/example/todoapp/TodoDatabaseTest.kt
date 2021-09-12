8ca0489e47d5868beabfcc6a4961b03eb23b6721
Gabriel Willans <g.willans@outlook.com>
2021-09-12 19:38:22 -0300

    Unit tests to Room database implementation

--- app/src/androidTest/java/com/example/todoapp/TodoDatabaseTest.kt
+++ app/src/androidTest/java/com/example/todoapp/TodoDatabaseTest.kt
@@ -6,8 +6,9 @@ import androidx.test.platform.app.InstrumentationRegistry
 import com.example.todoapp.database.ToDoDatabase
 import com.example.todoapp.database.ToDoDatabaseDao
 import com.example.todoapp.database.ToDoEntity
+import kotlinx.coroutines.runBlocking
 import org.junit.After
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -41,11 +42,13 @@ class TodoDatabaseTest {
 
     @Test
     @Throws(Exception::class)
-    fun insertAndGetBetweenDates() {
-        val todo = ToDoEntity()
+    fun insertAndGetBetweenDates() = runBlocking {
+        val calendar = Calendar.getInstance()
+        calendar.set(2021,8,2,12,0,0)
+
+        val todo = ToDoEntity(date=calendar.timeInMillis)
         toDoDao.insert(todo)
 
-        val calendar = Calendar.getInstance()
         calendar.set(2021,8,2,0,0,0)
         val startDate = calendar.timeInMillis
         calendar.set(2021,8,2,23,59,59)
@@ -54,17 +57,53 @@ class TodoDatabaseTest {
         Log.i("teste","$todo - ${todos}")
         Log.i("teste","$startDate - ${endDate}")
         Log.i("teste",todos?.get(0)?.title?:"nada")
-        assertEquals(todos?.get(0)?.title, "title")
+        assertEquals("title",todos?.get(0)?.title )
     }
 
     @Test
     @Throws(Exception::class)
-    fun insertAndGetAll() {
+    fun insertAndGetAll() = runBlocking {
         val todo = ToDoEntity()
         toDoDao.insert(todo)
 
         val todos = toDoDao.getAll()
         Log.i("teste","$todo - ${todos}")
-        assertEquals(todos?.get(0)?.title, "title")
+        assertEquals("title",todos?.get(0)?.title)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertUpdateAndGetToDo() = runBlocking {
+        val originTodo = ToDoEntity()
+        toDoDao.insert(originTodo)
+
+        val updateTodo = toDoDao.getAll()?.get(0)?.copy(title = "updated To-Do")
+            ?:throw Throwable("Test failed")
+
+        toDoDao.update(updateTodo)
+
+        val todos = toDoDao.getAll()
+        Log.i("teste","$originTodo - ${todos}")
+        assertTrue(todos?.any {
+            it.title == "updated To-Do"
+        }?:false)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertDeleteAndGetToDo() = runBlocking {
+        val todo = ToDoEntity(title = "deleted To-Do")
+        toDoDao.insert(todo)
+
+        val deleteTodo = toDoDao.getAll()?.filter { it.title == "deleted To-Do"}?.get(0)
+            ?:throw Throwable("Test failed")
+
+        toDoDao.delete(deleteTodo)
+
+        val todos = toDoDao.getAll()
+        Log.i("teste","$todo - ${todos}")
+        assertFalse(todos?.any {
+            it.title == "deleted To-Do"
+        }?:true)
     }
 }
